2c0b3aa0cceddebb984a6116556290089a13d61b
jinil <ytonyyun@naver.com>
2021-10-12 14:23:29 +0900

    [윤진일]

---
+++ src/main/java/com/lytear/lytear/ex/HelloController.java
@@ -0,0 +1,27 @@
+package com.lytear.lytear.ex;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String helloWorld() {
+		return "Hello Hello World";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/com/lytear/lytear/LytearRealtyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lytear.lytear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LytearRealtyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
